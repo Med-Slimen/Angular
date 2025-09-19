e2ecf302be0fe57747235640bc2792454f6fa415
Med-Slimen <mohamedamineslimene01@gmail.com>
2025-09-19 17:56:35 +0100

    initial

--- src/app/binding/binding.ts
+++ src/app/binding/binding.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-binding',
   imports: [],
+  standalone:true,
   templateUrl: './binding.html',
   styles: ``
 })
 export class Binding {
-
+  titre : String = "String Interpolation";
+  id : String="Property Binding";
 }
