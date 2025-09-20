ffddfa7b2b8b6e4a4b3b0082574ce5a572d667c3
Med-Slimen <mohamedamineslimene01@gmail.com>
2025-09-20 15:05:04 +0100

    produit componenet

--- Udemy course/section_5_6_7_8/MesProduit/src/app/app.routes.ts
+++ Udemy course/section_5_6_7_8/MesProduit/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Produit } from './produit/produit';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"produit",component:Produit}
+];
---
+++ Udemy course/section_5_6_7_8/MesProduit/src/app/produit/produit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produit',
+  imports: [],
+  standalone:true,
+  templateUrl: './produit.html',
+})
+export class Produit {
+
+}
