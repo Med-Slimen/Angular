72da134635ae10c6cc2781d70444c836737e05ed
Med-Slimen <mohamedamineslimene01@gmail.com>
2025-09-26 22:46:01 +0100

    section 9/10

---
+++ Udemy course/section 9/MesProduit/src/app/produit/produit.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produit',
+  imports: [],
+  standalone:true,
+  templateUrl: './produit.html',
+})
+export class Produit {
+  produits : String[];
+  constructor(){
+    this.produits=["PC Asus","Imprimante","TV","Ecran"]
+  }
+}
