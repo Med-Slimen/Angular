624a1d84bc6e9a6817db2a7e7f819ba06813559f
Med-Slimen <mohamedamineslimene01@gmail.com>
2025-10-03 14:18:33 +0100

    navigate add-produit

--- Udemy course/section 9_10/MesProduit/src/app/add-produit/add-produit.ts
+++ Udemy course/section 9_10/MesProduit/src/app/add-produit/add-produit.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../service/produit';
+import {Router } from '@angular/router';
 @Component({
   selector: 'app-add-produit',
   imports: [FormsModule],
@@ -10,14 +11,12 @@ import { ProduitService } from '../service/produit';
 export class AddProduit implements OnInit {
   newProduit = new Produit();
   msg! : String;
-  constructor(private ProduitService : ProduitService){
+  constructor(private ProduitService : ProduitService,private route : Router){
   }
   ngOnInit(): void {
   }
   addProduit():void{
     this.ProduitService.addProduit(this.newProduit);
-    this.msg="Produit "+this.newProduit.nomProduit+" ajouté avec successé"
-    alert(this.msg)
-    this.newProduit= new Produit();
+    this.route.navigate(['produit']);
   }
 }
