961336c51f449c30e5f46dfa2bb309815ae1b853
Med-Slimen <mohamedamineslimene01@gmail.com>
2025-11-08 21:21:07 +0100

    dv 7

---
+++ Udemy course/devoir 7/MesProduit/src/app/add-produit/add-produit.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../service/produit';
+import {Router } from '@angular/router';
+@Component({
+  selector: 'app-add-produit',
+  imports: [FormsModule],
+  templateUrl: './add-produit.html',
+})
+export class AddProduit implements OnInit {
+  newProduit = new Produit();
+  msg! : String;
+  constructor(private ProduitService : ProduitService,private route : Router){
+  }
+  ngOnInit(): void {
+  }
+  addProduit():void{
+    this.ProduitService.addProduit(this.newProduit);
+    this.route.navigate(['produit']);
+  }
+}
